d98e53dd1e035cb20fbb2b86125120463e04de36
qingmeijiupiao <2368357055@qq.com>
2024-07-13 22:16:14 +0800

    add file

--- 复用模块/OPS-9/ops9.hpp
+++ 复用模块/OPS-9/ops9.hpp
@@ -1,137 +1,121 @@
-#ifndef SBUS_HPP
-#define SBUS_HPP
-#include <Arduino.h>
+/*
+ * @Description: 
+ * @Author: qingmeijiupiao
+ * @Date: 2024-04-23 17:01:25
+ */
+#ifndef OPS9_HPP
+#define OPS9_HPP
+#include "HardwareSerial.h"
+void ops9_task(void *n);
+class ops9 {
+	friend void ops9_task(void *n);
 
-class SBUS {
   public:
-	SBUS(uint8_t pin, HardwareSerial *serial) {
-		_pin = pin;
-		_serial = serial;
-	};
-	SBUS(uint8_t pin) {
-		_pin = pin;
-		_serial = &Serial2;
-	};
-	SBUS() {
-		_serial = &Serial2;
-		_pin = 6;
-	};
+	ops9(HardwareSerial *serial) { _serial = serial; };
+	~ops9(){};
+	void setup(uint8_t TX_pin, uint8_t RX_pin) {
+		_serial->begin(115200, SERIAL_8N1, RX_pin, TX_pin);
+		xTaskCreate(ops9_task, "read_data", 4096, this, 2, NULL);
+	}
 	void setup() {
-		pinMode(_pin, INPUT);
-		_serial->begin(100000, SERIAL_8E2, _pin, 38);
-		memset(raw_data, 0, sizeof(raw_data));
-		memset(channel_data, 0, sizeof(channel_data));
-		flag = 0;
-		xTaskCreate(loop_task, "loop_task", 4096, this, 2, NULL);
+		_serial->begin(115200);
+		xTaskCreate(ops9_task, "read_data", 4096, this, 2, NULL);
+	}
+	//校准
+	void calibrate() { _serial->print("ACTR"); }
+	//坐标清零
+	void re_zero() { _serial->print("ACT0"); }
+	//重新设置航向角
+	void reset_yaw(float Yaw) {
+		_serial->print("ACTJ");
+		_serial->write((uint8_t *)&Yaw, 4);
+	}
+	void reset_x(float X) {
+		_serial->print("ACTX");
+		_serial->write((uint8_t *)&X, 4);
+	}
+	void reset_y(float Y) {
+		_serial->print("ACTY");
+		_serial->write((uint8_t *)&Y, 4);
+	}
+	void reset_X_Y(float X, float Y) {
+		_serial->print("ACTD");
+		_serial->write((uint8_t *)&X, 4);
+		_serial->write((uint8_t *)&Y, 4);
+	}
+	void reset_yaw_x_y(float Yaw, float X, float Y) {
+		_serial->print("ACTD");
+		_serial->write((uint8_t *)&Yaw, 4);
+		_serial->write((uint8_t *)&X, 4);
+		_serial->write((uint8_t *)&Y, 4);
+	}
+	float getYaw() { return yaw; }
+	float getPitch() { return pitch; }
+	float getRoll() { return roll; }
+	float getX() { return x; }
+	float getY() { return y; }
+
+  private:
+	static void ops9_task(void *n) {
+		ops9 *obj = (ops9 *)n;
+		while (1) {
+			obj->read_data();
+		}
 	};
-	static void loop_task(void *p) {
-		SBUS *obj = (SBUS *)p;
-		while (true) {
-			if (obj->_serial->available() < 25) {
-				delay(1);
-				continue;
-			}
+	void read_data() {
+		uint8_t data[26];
+		uint8_t front_data[2];
 
-			while (obj->_serial->peek() != 0xf) {
-				obj->_serial->read();
-				delay(1);
-			}
-			while (obj->_serial->available() < 25) {
-				delay(1);
-			}
-			obj->_serial->readBytes(obj->raw_data, 25);
-			if (obj->raw_data[0] != 0x0f) {
-				continue;
-			}
-			int check = ((int16_t)obj->raw_data[1] >> 0 |
-			                ((int16_t)obj->raw_data[2] << 8)) &
-			            0x07FF;
-			uint16_t channel_data_check[16];
-
-			//每11位为一个通道
-			channel_data_check[0] = ((int16_t)obj->raw_data[1] >> 0 |
-			                            ((int16_t)obj->raw_data[2] << 8)) &
-			                        0x07FF;
-			channel_data_check[1] = ((int16_t)obj->raw_data[2] >> 3 |
-			                            ((int16_t)obj->raw_data[3] << 5)) &
-			                        0x07FF;
-			channel_data_check[2] = ((int16_t)obj->raw_data[3] >> 6 |
-			                            ((int16_t)obj->raw_data[4] << 2) |
-			                            (int16_t)obj->raw_data[5] << 10) &
-			                        0x07FF;
-			channel_data_check[3] = ((int16_t)obj->raw_data[5] >> 1 |
-			                            ((int16_t)obj->raw_data[6] << 7)) &
-			                        0x07FF;
-			channel_data_check[4] = ((int16_t)obj->raw_data[6] >> 4 |
-			                            ((int16_t)obj->raw_data[7] << 4)) &
-			                        0x07FF;
-			channel_data_check[5] = ((int16_t)obj->raw_data[7] >> 7 |
-			                            ((int16_t)obj->raw_data[8] << 1) |
-			                            (int16_t)obj->raw_data[9] << 9) &
-			                        0x07FF;
-			channel_data_check[6] = ((int16_t)obj->raw_data[9] >> 2 |
-			                            ((int16_t)obj->raw_data[10] << 6)) &
-			                        0x07FF;
-			channel_data_check[7] = ((int16_t)obj->raw_data[10] >> 5 |
-			                            ((int16_t)obj->raw_data[11] << 3)) &
-			                        0x07FF;
-			channel_data_check[8] = ((int16_t)obj->raw_data[12] >> 0 |
-			                            ((int16_t)obj->raw_data[13] << 8)) &
-			                        0x07FF;
-			channel_data_check[9] = ((int16_t)obj->raw_data[13] >> 3 |
-			                            ((int16_t)obj->raw_data[14] << 5)) &
-			                        0x07FF;
-			channel_data_check[10] = ((int16_t)obj->raw_data[14] >> 6 |
-			                             ((int16_t)obj->raw_data[15] << 2) |
-			                             (int16_t)obj->raw_data[16] << 10) &
-			                         0x07FF;
-			channel_data_check[11] = ((int16_t)obj->raw_data[16] >> 1 |
-			                             ((int16_t)obj->raw_data[17] << 7)) &
-			                         0x07FF;
-			channel_data_check[12] = ((int16_t)obj->raw_data[17] >> 4 |
-			                             ((int16_t)obj->raw_data[18] << 4)) &
-			                         0x07FF;
-			channel_data_check[13] = ((int16_t)obj->raw_data[18] >> 7 |
-			                             ((int16_t)obj->raw_data[19] << 1) |
-			                             (int16_t)obj->raw_data[20] << 9) &
-			                         0x07FF;
-			channel_data_check[14] = ((int16_t)obj->raw_data[20] >> 2 |
-			                             ((int16_t)obj->raw_data[21] << 6)) &
-			                         0x07FF;
-			channel_data_check[15] = ((int16_t)obj->raw_data[21] >> 5 |
-			                             ((int16_t)obj->raw_data[22] << 3)) &
-			                         0x07FF;
-
-			bool is_ok = true;
-			for (int i = 0; i < 16; i++) {
-				if (channel_data_check[i] > 1700 ||
-				    channel_data_check[i] < 350) {
-					is_ok = false;
+		while (1) {
+			if (_serial->available() >= 28) {
+				while (1) {
+					front_data[0] = _serial->read();
+					front_data[1] = _serial->peek();
+					if (front_data[0] == 0x0d && front_data[1] == 0x0a) {
+						_serial->read();
+						break;
+					}
+					delay(1);
+				}
+				_serial->read(data, 26);
+				if (data[24] == 0x0a && data[25] == 0x0d) {
+					int temp = data[0] + (data[1] << 8) + (data[2] << 16) +
+					           (data[3] << 24);
+					int *temp2 = &temp;
+					yaw = *(float *)temp2;
+					temp = data[4] + (data[5] << 8) + (data[6] << 16) +
+					       (data[7] << 24);
+					pitch = *(float *)temp2;
+					temp = data[8] + (data[9] << 8) + (data[10] << 16) +
+					       (data[11] << 24);
+					roll = *(float *)temp2;
+					temp = data[12] + (data[13] << 8) + (data[14] << 16) +
+					       (data[15] << 24);
+					x = *(float *)temp2;
+					temp = data[16] + (data[17] << 8) + (data[18] << 16) +
+					       (data[19] << 24);
+					y = *(float *)temp2;
+					temp = data[20] + (data[21] << 8) + (data[22] << 16) +
+					       (data[23] << 24);
+					yaw_speed = *(float *)temp2;
 				}
 			}
-
-			if (!is_ok) {
-				continue;
-			}
-
-			memcpy(obj->channel_data, channel_data_check, 16 * 2);
-			obj->flag = obj->raw_data[24];
-			if (obj->is_first) {
-				obj->is_first = false;
-			}
+			delay(1);
 		}
 	}
-	uint16_t operator[](uint8_t i) { return channel_data[i - 1]; }
-	uint16_t get_channel_data(uint8_t i) { return channel_data[i - 1]; }
-	uint8_t get_flag() { return flag; }
-	bool is_first = true;
-
-  protected:
-	uint8_t _pin;
 	HardwareSerial *_serial;
-	uint8_t raw_data[25];
-	uint16_t channel_data[16];
-	uint8_t flag;
+	//航向角
+	float yaw = 0;
+	//俯仰角
+	float pitch = 0;
+	//横滚角
+	float roll = 0;
+	//X坐标
+	float x = 0;
+	//Y坐标
+	float y = 0;
+	//航向角速度
+	float yaw_speed = 0;
 };
 
 #endif
